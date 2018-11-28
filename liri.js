e7db1cd3501140c6cf8d46972ec54c65b7b906a3
Pwneill <phelan.oneill@gmail.com>
2018-11-27 18:17:07 -0600

    finished bandsintown, begun work on omdb and streamlined checking if input is null

--- liri.js
+++ liri.js
@@ -8,14 +8,19 @@ var Spotify = require("node-spotify-api");
 
 var spotify = new Spotify(keys.spotify);
 
-var moment = require("moment")
+var moment = require("moment");
 
 var app = process.argv[2];
 
 var input = process.argv[3];
 
+var songQuery = "";
+
+var movieQuery = "";
 
 function userApp() {
+  checkInput();
+
   switch (app) {
     case "spotify-this-song":
       spotifyAPI();
@@ -23,38 +28,55 @@ function userApp() {
     case "concert-this":
       bandsInTownAPI();
       break;
+    case "movie-this":
+      omdbAPI();
+      break;
   }
 }
 
 function bandsInTownAPI() {
-  concertArr = []
   axios
-  .get("https://rest.bandsintown.com/artists/"+input+"/events?app_id=codingbootcamp").then(
-    function(response) {
-      response.data.forEach(function(concert) {
-        concertArr.push(concert)
-        console.log(concertArr.length)
-      })
-    }
-  );
-  
-  // This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        input +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      response.data.forEach(function(result) {
+        var concert = result.venue;
+        var date = moment(result.datetime).format("MM/DD/YYYY");
+        console.log(
+          "\n ---------------------------------------\n" +
+            "Venue: " +
+            concert.name +
+            "\n" +
+            "Location: " +
+            concert.city +
+            " " +
+            concert.region +
+            " " +
+            concert.country +
+            "\n" +
+            "Date: " +
+            date +
+            "\n ---------------------------------------\n"
+        );
+      });
+    });
+}
 
-  //    * Name of the venue
-
-  //    * Venue location
-
-  //    * Date of the Event (use moment to format this as "MM/DD/YYYY")
+// checks to see if input is null and assigns a default value if it is
+function checkInput() {
+  if (input) {
+    songQuery = input;
+    movieQuery = input;
+  } else {
+    songQuery = "The Sign Ace of Base";
+    movieQuery = "Mr. Nobody";
+  }
 }
 
 function spotifyAPI() {
-  var songQuery = "";
-  if (input) {
-    songQuery = input;
-  } else {
-    songQuery = "The Sign Ace of Base";
-  }
-
   spotify
     .search({
       type: "track",
@@ -64,7 +86,6 @@ function spotifyAPI() {
     .then(function(response, err) {
       if (err) console.log(err);
       var songArr = response.tracks.items[0];
-      console.log(songArr);
       console.log(
         "\n ---------------------------------------\n" +
           "Artist: " +
@@ -82,4 +103,25 @@ function spotifyAPI() {
       );
     });
 }
-userApp()
+
+function omdbAPI() {
+  axios.get(
+    "http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&apikey=trilogy").then(function(response) {
+      console.log(response.data)
+    });
+}
+
+userApp();
+
+// This will output the following information to your terminal/bash window:
+
+// ```
+//   * Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+// ```
