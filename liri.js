4939c27958b7e39765983b5af71e6ac14691d733
Pwneill <phelan.oneill@gmail.com>
2018-12-01 14:01:56 -0600

    fixed issues with inquirer; allowed do-what-it-says to skip input prompt

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@ var axios = require("axios");
 
 var fs = require("fs");
 
-var inquirer = require('inquirer');
+var inquirer = require("inquirer");
 
 var Spotify = require("node-spotify-api");
 
@@ -18,62 +18,87 @@ var songQuery = "";
 
 var movieQuery = "";
 
+var bandQuery = "";
+
 var newLine = "\n ---------------------------------------\n";
 
-var input = ''
+var input = "";
 
-function selection () {
+function selection() {
   inquirer
-  .prompt([
+    .prompt([
       {
         type: "list",
-        message: "Welcome to LIRI Bot! Please select one of the following options",
-        choices: ["spotify-this-song", "concert-this", "movie-this", "do-what-it-says"],
+        message:
+          "Welcome to LIRI Bot! Please select one of the following options",
+        choices: [
+          "spotify-this-song",
+          "concert-this",
+          "movie-this",
+          "do-what-it-says"
+        ],
         name: "app"
-      },
-      {
-        name: "input",
-        message: "What would you like to search for?"
       }
-    ]).then (function (answers) {
-      input = answers.input
-      checkInput()
-
-      switch (answers.app) {
-        case "spotify-this-song":
-          spotifyAPI();
-          break;
-        case "concert-this":
-          bandsInTownAPI();
-          break;
-        case "movie-this":
-          omdbAPI();
-          break;
-        case "do-what-it-says":
-          doRandom();
-          break;
-    }
-  })
+    ])
+    .then(function(answers) {
+      if (answers.app === "do-what-it-says") {
+        doRandom();
+        return;
+      } else {
+         app = answers.app;
+        inquirer
+          .prompt([
+            {
+              message: "What would you like to search?",
+              name: "input"
+            }
+          ])
+          .then(function(response) {
+            input = response.input;
+            appSelect(app);
+          });
+      }
+    });
 }
 
+function appSelect(app) {
+  checkInput();
 
-// checks to see if input is null and assigns a default value if it is
+  switch (app) {
+    case "spotify-this-song":
+      spotifyAPI();
+      break;
+    case "concert-this":
+      bandsInTownAPI();
+      break;
+    case "movie-this":
+      omdbAPI();
+      break;
+  }
+}
+
+// // checks to see if input is null and assigns a default value if it is
 function checkInput() {
   if (input) {
     songQuery = input;
     movieQuery = input;
+    bandQuery = input;
   } else {
     songQuery = "The Sign Ace of Base";
     movieQuery = "Mr. Nobody";
+    bandQuery = "Pixies";
   }
 }
 
-function timestamp () {
+function timestamp() {
+  var time = moment()
+    .local()
+    .format("MM/DD/YYYY HH:mm");
+  var timeStamp = String(
+    "\n Search Time: " + time + "\n" + " Search Parameter: " + input
+  );
 
-  var time = moment().local().format("MM/DD/YYYY HH:mm")
-  var timeStamp = String("\n Search Time: " + time + "\n" + " Search Parameter: " + input)
-
-  fs.appendFileSync("./log_file/log.txt", timeStamp, function(err) {
+  fs.appendFileSync("./log.txt", timeStamp, function(err) {
     if (err) throw err;
   });
 }
@@ -82,30 +107,28 @@ function logEntry(entry) {
   var newEntry = Object.values(entry);
   newEntry = String(newEntry).replace(/,/g, " ");
 
-  fs.appendFileSync("./log_file/log.txt", newLine, function(err) {
+  fs.appendFileSync("./log.txt", newLine, function(err) {
     if (err) throw err;
   });
-  fs.appendFileSync("./log_file/log.txt", newEntry, function(err) {
+  fs.appendFileSync("./log.txt", newEntry, function(err) {
     if (err) throw err;
   });
-  fs.appendFileSync("./log_file/log.txt", newLine, function(err) {
+  fs.appendFileSync("./log.txt", newLine, function(err) {
     if (err) throw err;
-  })
+  });
   console.log("Your data was written to file!");
 }
 
 function bandsInTownAPI() {
-
-  timestamp()
+  timestamp();
 
   axios
     .get(
       "https://rest.bandsintown.com/artists/" +
-        input +
+        bandQuery +
         "/events?app_id=codingbootcamp"
     )
     .then(function(response) {
-
       response.data.forEach(function(result) {
         var concert = result.venue;
         var date = moment(result.datetime).format("MM/DD/YYYY");
@@ -113,11 +136,14 @@ function bandsInTownAPI() {
         var concertEntry = {
           Venue: " Venue: " + concert.name,
           Location:
-          "\n " +
-          concert.city + " " +
-          concert.region + " " +
-          concert.region + " " +
-          concert.country,
+            "\n " +
+            concert.city +
+            " " +
+            concert.region +
+            " " +
+            concert.region +
+            " " +
+            concert.country,
           Date: "\n " + date
         };
 
@@ -125,18 +151,17 @@ function bandsInTownAPI() {
 
         console.log(
           newLine +
-          concertEntry.Venue +
-          concertEntry.Location +
-          concertEntry.Date +
-          newLine
+            concertEntry.Venue +
+            concertEntry.Location +
+            concertEntry.Date +
+            newLine
         );
       });
     });
 }
 
 function spotifyAPI() {
-
-  timestamp()
+  timestamp();
 
   spotify
     .search({
@@ -159,17 +184,16 @@ function spotifyAPI() {
 
       console.log(
         newLine +
-        songEntry.Artist +
-        songEntry.Song_Name +
-        songEntry.Preview_URL +
-        songEntry.Album +
-        newLine
+          songEntry.Artist +
+          songEntry.Song_Name +
+          songEntry.Preview_URL +
+          songEntry.Album +
+          newLine
       );
     });
 }
 
 function omdbAPI() {
-
   axios
     .get(
       "http://www.omdbapi.com/?t=" +
@@ -189,20 +213,20 @@ function omdbAPI() {
         Summary: "\n Summary: " + movie.Plot,
         Actors: "\n Starring: " + movie.Actors
       };
-      
-      timestamp()
+
+      timestamp();
       logEntry(movieEntry);
 
       console.log(
         newLine +
-        movieEntry.Title +
-        movieEntry.Year +
-        movieEntry.IMDB_Rating +
-        movieEntry.Rotten_Tomatoes_Rating +
-        movieEntry.Filmed_in +
-        movieEntry.Summary +
-        movieEntry.Actors +
-        newLine
+          movieEntry.Title +
+          movieEntry.Year +
+          movieEntry.IMDB_Rating +
+          movieEntry.Rotten_Tomatoes_Rating +
+          movieEntry.Filmed_in +
+          movieEntry.Summary +
+          movieEntry.Actors +
+          newLine
       );
     });
 }
@@ -215,9 +239,10 @@ function doRandom() {
     } else {
       randomArr = data.split(",");
       input = randomArr[1];
-      userApp(randomArr[0]);
+      app = randomArr[0]
+      appSelect(app);
     }
   });
 }
 
-selection()
+selection();
